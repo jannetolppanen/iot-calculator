740b63be00a5d502e97099d4333246035ca3fc3d
Janne Tolppanen <janne.ks@gmail.com>
2023-09-28 13:16:01 +0300

    changed logic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
@@ -7,51 +7,54 @@ const port = 3000;
 let totalVisitors = 0;
 
 // total number of visitors inside area currently
-let currentVisitors = 0
+let currentVisitors = 0;
 
 // credentials
-const auth_username = "iotcalc"
-const auth_password = "secret"
+const auth_username = 'iotcalc';
+const auth_password = 'secret';
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/status', (req, res) => {
-  res.status(200).json({ currentVisitors, totalVisitors })
-})
+  res.status(200).json({ currentVisitors, totalVisitors });
+});
 
 app.post('/update', (req, res) => {
-  const { username, password, currentCount, totalCount } = req.body
+  const { username, password, currentCount, totalCount } = req.body;
 
   if (auth_username !== username || auth_password !== password) {
-    return res.status(401).json({ "error" : "invalid credentials" })
+    return res.status(401).json({ error: 'invalid credentials' });
   }
-  
+
   if (currentCount === undefined || totalCount === undefined) {
-    return res.status(400).json({ "error" : "data missing" })
+    return res.status(400).json({ error: 'data missing' });
   }
 
-  if (currentCount === null || totalCount === null || isNaN(currentCount) || isNaN(totalCount)) {
-    return res.status(400).json({ "error": "invalid data" })
+  if (
+    currentCount === null ||
+    totalCount === null ||
+    isNaN(currentCount) ||
+    isNaN(totalCount)
+  ) {
+    return res.status(400).json({ error: 'invalid data' });
   }
 
-  currentVisitors = currentVisitors + currentCount;
-  totalVisitors = totalVisitors + totalCount
-  res.status(200).json({ currentVisitors, totalVisitors })
-})
-
+  currentVisitors = currentCount;
+  totalVisitors = totalCount;
+  res.status(200).json({ currentVisitors, totalVisitors });
+});
 
 app.post('/reset', (req, res) => {
-  const { username, password } = req.body
+  const { username, password } = req.body;
   if (auth_username !== username || auth_password !== password) {
-    return res.status(401).json({ "error" : "invalid credentials" })
+    return res.status(401).json({ error: 'invalid credentials' });
   }
-  totalVisitors = 0
-  currentVisitors = 0
-  res.status(200).json({ currentVisitors, totalVisitors })
-})
-
+  totalVisitors = 0;
+  currentVisitors = 0;
+  res.status(200).json({ currentVisitors, totalVisitors });
+});
 
 app.listen(port, 'localhost', () => {
-  console.log(`listening at localhost:${port}`)
+  console.log(`listening at localhost:${port}`);
 });
